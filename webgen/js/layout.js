c36b850b6d2fa1523df7630267f9b20cd4199813
Arun Persaud <arun@nubati.net>
2012-10-21 12:44:41 -0700

    added bootstrap files and modifyed css and layout a bit
    to keep the html clean almost all the bootstrap styling is done in js/layout.js by adding the correct clases and wrappers to the html we use

--- webgen/js/layout.js
+++ webgen/js/layout.js
@@ -10,3 +10,22 @@
  		}});
     }
   );
+
+/* add bootstrap layout */
+$("div.everything").addClass('row');
+$("nav").wrap('<div class="span2">');
+$(".main").wrap('<div class="span9 offset1">');
+
+$("header").addClass('page-header');
+
+
+$(".main table").wrap('<div class="row">').wrap('<div class="offset1 span8">');
+$("table").addClass('table');
+$("table").addClass('table-condensed');
+$("table").addClass('table-hover');
+
+$("tr.disclaim").addClass('success');
+$("tr.nodisclaim").addClass('error');
+$("tr.external").addClass('warning');
+$("tr.external-present").addClass('info');
+
